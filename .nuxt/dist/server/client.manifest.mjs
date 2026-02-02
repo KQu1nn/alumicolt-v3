b571b7a035b48bf175b555852f494a9e1469d82a
KQu1nn <themastertechbr@gmail.com>
2026-02-02 20:16:06 -0300

    fix

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,4 +0,0 @@
-
-import { viteNodeFetch } from "file:///C:/Alumicolt-v2-main/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
-export default () => viteNodeFetch.getManifest()
-    
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///C:/Alumicolt-v2-main/node_modules/@nuxt/vite-builder/dist/vite-node-entry.mjs"
